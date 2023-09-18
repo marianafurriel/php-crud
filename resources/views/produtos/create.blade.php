296a374d3db43f354a8eac832ef0e510f9a05a4c
Mariana Furriel <marianafurrielss@gmail.com>
2023-09-18 18:15:07 -0300

    Model e create view de produto criada

---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Cadastrar novo produto</title>
+</head>
+<body>
+  <form action="">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome">
+
+    <label for="custo">Custo</label>
+    <input type="text" name="custo" id="custo">
+
+    <label for="preco">Preço</label>
+    <input type="text" name="preco" id="preco">
+
+    <label for="quantidade">Quantidade</label>
+    <input type="text" name="quantidade" id="quantidade">
+    <button>Salvar</button>
+  </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/produtos/novo', [ProdutosController::class, 'create']);
+Route::get('/produtos/novo', [ProdutosController::class, 'create'])->name('product.create');
